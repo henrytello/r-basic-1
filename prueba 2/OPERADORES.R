c47a61459dd2869f21e1f913afc007f2821715ba
nicole <xixhenry18@hotmail.com>
2020-06-02 17:27:21 -0500

    funciones
    funciones

---
+++ prueba 2/OPERADORES.R
@@ -0,0 +1,14 @@
+3+3
+pi
+sqrt(9)
+365%/%7
+log(pi)
+factorial(4)
+choose(4,1)
+edad=35
+nombre="juan"
+edad
+nombre
+edad, nombre
+edad;nombre
+edad & nombre
---
+++ prueba 2/variables y funciones.R
@@ -0,0 +1,11 @@
+##Funcion F(X)=X^3-3^X*SEN(X)
+f=function(x){
+  x^3-3^x*sin(x)
+}
+f(4) ## 4^3-3^4*sin(4)
+f(4)*f(5)
+
+mas<-function(t){
+  t+1;
+}
+mas(5)
